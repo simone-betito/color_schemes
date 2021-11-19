ea81a309576d9de0fcffbb48ce4c9df51f50deb0
Simone Betito <simone.betito@gmail.com>
2021-11-19 14:56:47 -0500

    changes

--- main.js
+++ main.js
@@ -17,7 +17,48 @@ async function loadData() {
   });
 
   console.log(colorConvert);
+  //const xAccessor = d => d.brightness
+  //const yAccessor = d => d.saturation
+
+  const width = d3.min([window.innerWidth * 0.9, window.innerHeight * 0.9]);
+
+  let dimensions = {
+    width: width,
+    height: width,
+    margin: {
+      top: 10,
+      right: 10,
+      bottom: 50,
+      left: 50,
+    },
+  };
+
+  dimensions.boundedWidth =
+    dimensions.width - dimensions.margin.left - dimensions.margin.right;
+  dimensions.boundedHeight =
+    dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
+
+  const wrapper = d3
+    .select("#wrapper")
+    .append("svg")
+    .attr("width", dimensions.width)
+    .attr("height", dimensions.height);
+
+  const bounds = wrapper
+    .append("g")
+    .style(
+      "transform",
+      `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`
+    );
+  const xScale = d3.scaleLinear();
+  const yScale = d3.scaleLinear();
+
+  bounds
+    .append("rect")
+    .attr("x", dimensions.boundedHeight / 2)
+    .attr("y", dimensions.boundedHeight / 2);
 }
+
 loadData();
 
 // https://www.w3schools.com/colors/colors_converter.asp
